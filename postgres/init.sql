97b64a0b55f573ec62280251c5b5e4c5f45bd730
Mikhail Ivashchenko <52649381+mikhail0090025@users.noreply.github.com>
2025-09-02 11:31:08 +0200

    Intermediate commit

---
+++ postgres/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    born_date DATE NOT NULL,
+    registration_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
